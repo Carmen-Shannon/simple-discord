503a1d610866f47096ea5bf678b8dae00c1beb3e
Carmen Shannon <carmen.shannon16@gmail.com>
2024-08-14 01:22:35 -0400

    remove unneeded struct

--- structs/gateway_presence_update.go
+++
@@ -1,8 +0,0 @@
-package structs
-
-type GatewayPresenceUpdate struct {
-	Since      int        `json:"since"`
-	Activities []Activity `json:"activities"`
-	Status     string     `json:"status"`
-	Afk        bool       `json:"afk"`
-}
