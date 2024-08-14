be0776bda9dc90635a0f1ed8d39969c636430389
Carmen Shannon <carmen.shannon16@gmail.com>
2024-08-14 01:33:45 -0400

    add tags

--- structs/guild.go
+++ structs/guild.go
@@ -105,47 +105,61 @@ type WelcomeScreen struct {
 }
 
 type Guild struct {
-	ID                          Snowflake
-	Name                        string
-	Icon                        *string
-	IconHash                    *string
-	Splash                      *string
-	DiscoverySplash             *string
-	Owner                       *bool
-	OwnerID                     Snowflake
-	Permissions                 *string
-	Region                      *VoiceRegion
-	AFKChannelID                *Snowflake
-	AFKTimeout                  int
-	IsWidgetEnabled             *bool
-	WidgetChannelID             *Snowflake
-	VerificationLevel           VerificationLevel
-	DefaultMessageNotifications DefaultMessageNotificationLevel
-	ExplicitContentFilter       ExplicitContentFilterLevel
-	Roles                       []Role
-	Emojis                      []Emoji
-	Features                    []GuildFeature
-	MFALevel                    MFALevel
-	ApplicationID               *Snowflake
-	SystemChannelID             *Snowflake
-	SystemChannelFlags          SystemChannelFlag
-	RulesChannelID              *Snowflake
-	MaxPresences                *int
-	MaxMembers                  *int
-	VanityURLCode               *string
-	Description                 *string
-	Banner                      *string
-	PremiumTier                 PremiumTier
-	PremiumSubscriptionCount    *int
-	PreferredLocale             string
-	PublicUpdatesChannelID      *Snowflake
-	MaxVideoChannelUsers        *int
-	MaxStageVideoChannelUsers   *int
-	ApproximateMemberCount      *int
-	ApproximatePresenceCount    *int
-	WelcomeScreen               *WelcomeScreen
-	NSFWLevel                   NSFWLevel
-	Stickers                    []Sticker
-	IsPremiumProgressBarEnabled bool
-	SafetyAlertsChannelID       *Snowflake
+	ID                          Snowflake                       `json:"id"`
+	Name                        string                          `json:"name"`
+	Icon                        *string                         `json:"icon,omitempty"`
+	IconHash                    *string                         `json:"icon_hash,omitempty"`
+	Splash                      *string                         `json:"splash,omitempty"`
+	DiscoverySplash             *string                         `json:"discovery_splash,omitempty"`
+	Owner                       *bool                           `json:"owner,omitempty"`
+	OwnerID                     Snowflake                       `json:"owner_id"`
+	Permissions                 *string                         `json:"permissions,omitempty"`
+	Region                      *VoiceRegion                    `json:"region,omitempty"`
+	AFKChannelID                *Snowflake                      `json:"afk_channel_id,omitempty"`
+	AFKTimeout                  int                             `json:"afk_timeout"`
+	IsWidgetEnabled             *bool                           `json:"is_widget_enabled,omitempty"`
+	WidgetChannelID             *Snowflake                      `json:"widget_channel_id,omitempty"`
+	VerificationLevel           VerificationLevel               `json:"verification_level"`
+	DefaultMessageNotifications DefaultMessageNotificationLevel `json:"default_message_notifications"`
+	ExplicitContentFilter       ExplicitContentFilterLevel      `json:"explicit_content_filter"`
+	Roles                       []Role                          `json:"roles"`
+	Emojis                      []Emoji                         `json:"emojis"`
+	Features                    []GuildFeature                  `json:"features"`
+	MFALevel                    MFALevel                        `json:"mfa_level"`
+	ApplicationID               *Snowflake                      `json:"application_id,omitempty"`
+	SystemChannelID             *Snowflake                      `json:"system_channel_id,omitempty"`
+	SystemChannelFlags          SystemChannelFlag               `json:"system_channel_flags"`
+	RulesChannelID              *Snowflake                      `json:"rules_channel_id,omitempty"`
+	MaxPresences                *int                            `json:"max_presences,omitempty"`
+	MaxMembers                  *int                            `json:"max_members,omitempty"`
+	VanityURLCode               *string                         `json:"vanity_url_code,omitempty"`
+	Description                 *string                         `json:"description,omitempty"`
+	Banner                      *string                         `json:"banner,omitempty"`
+	PremiumTier                 PremiumTier                     `json:"premium_tier"`
+	PremiumSubscriptionCount    *int                            `json:"premium_subscription_count,omitempty"`
+	PreferredLocale             string                          `json:"preferred_locale"`
+	PublicUpdatesChannelID      *Snowflake                      `json:"public_updates_channel_id,omitempty"`
+	MaxVideoChannelUsers        *int                            `json:"max_video_channel_users,omitempty"`
+	MaxStageVideoChannelUsers   *int                            `json:"max_stage_video_channel_users,omitempty"`
+	ApproximateMemberCount      *int                            `json:"approximate_member_count,omitempty"`
+	ApproximatePresenceCount    *int                            `json:"approximate_presence_count,omitempty"`
+	WelcomeScreen               *WelcomeScreen                  `json:"welcome_screen,omitempty"`
+	NSFWLevel                   NSFWLevel                       `json:"nsfw_level"`
+	Stickers                    []Sticker                       `json:"stickers"`
+	IsPremiumProgressBarEnabled bool                            `json:"is_premium_progress_bar_enabled"`
+	SafetyAlertsChannelID       *Snowflake                      `json:"safety_alerts_channel_id,omitempty"`
+}
+
+type GuildPreview struct {
+	ID                       Snowflake      `json:"id"`
+	Name                     string         `json:"name"`
+	Icon                     *string        `json:"icon,omitempty"`
+	Splash                   *string        `json:"splash,omitempty"`
+	DiscoverySplash          *string        `json:"discovery_splash,omitempty"`
+	Emojis                   []Emoji        `json:"emojis"`
+	Features                 []GuildFeature `json:"features"`
+	ApproximateMemberCount   int            `json:"approximate_member_count"`
+	ApproximatePresenceCount int            `json:"approximate_presence_count"`
+	Description              *string        `json:"description,omitempty"`
+	Stickers                 []Sticker      `json:"stickers"`
 }
