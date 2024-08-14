f65480f7525fdd7dfb213f73408e71069f516739
Carmen Shannon <carmen.shannon16@gmail.com>
2024-08-14 15:35:15 -0400

    thank the lord for github copilot

--- structs/channel.go
+++ structs/channel.go
@@ -44,76 +44,76 @@ const (
 )
 
 type Channel struct {
-	ID                     Snowflake
-	Type                   ChannelType
-	GuildID                *Snowflake
-	Position               *int
-	PermissionOverwrites   *Overwrite
-	Name                   *string
-	Topic                  *string
-	NSFW                   *bool
-	LastMessageID          *Snowflake
-	Bitrate                *int
-	UserLimit              *int
-	RateLimitPerUser       *int
-	Recipients             []User
-	Icon                   *string
-	OwnerID                *Snowflake
-	ApplicationID          *Snowflake
-	Managed                *bool
-	ParentID               *Snowflake
-	LastPinTimestamp       *time.Time
-	RtcRegion              *string
-	VideoQualityMode       *int
-	MessageCount           *int
-	MemberCount            *int
-	ThreadMetadata         *ThreadMetaData
-	ThreadMember           *ThreadMember
-	AutoArchiveDuration    *int
-	Permissions            *string
-	Flags                  *ChannelFlag
-	TotalMessageSent       *int
-	AvailableTags          []ForumTag
-	AppliedTags            []Snowflake
-	DefaultReactionEmoji   *DefaultReaction
-	DefaultThreadRateLimit *int
-	DefaultSortOrder       *SortOrderType
-	DefaultForumLayout     *ForumLayoutType
+	ID                     Snowflake               `json:"id"`
+	Type                   ChannelType             `json:"type"`
+	GuildID                *Snowflake              `json:"guild_id,omitempty"`
+	Position               *int                    `json:"position,omitempty"`
+	PermissionOverwrites   *Overwrite              `json:"permission_overwrites,omitempty"`
+	Name                   *string                 `json:"name,omitempty"`
+	Topic                  *string                 `json:"topic,omitempty"`
+	NSFW                   *bool                   `json:"nsfw,omitempty"`
+	LastMessageID          *Snowflake              `json:"last_message_id,omitempty"`
+	Bitrate                *int                    `json:"bitrate,omitempty"`
+	UserLimit              *int                    `json:"user_limit,omitempty"`
+	RateLimitPerUser       *int                    `json:"rate_limit_per_user,omitempty"`
+	Recipients             []User                  `json:"recipients,omitempty"`
+	Icon                   *string                 `json:"icon,omitempty"`
+	OwnerID                *Snowflake              `json:"owner_id,omitempty"`
+	ApplicationID          *Snowflake              `json:"application_id,omitempty"`
+	Managed                *bool                   `json:"managed,omitempty"`
+	ParentID               *Snowflake              `json:"parent_id,omitempty"`
+	LastPinTimestamp       *time.Time              `json:"last_pin_timestamp,omitempty"`
+	RtcRegion              *string                 `json:"rtc_region,omitempty"`
+	VideoQualityMode       *int                    `json:"video_quality_mode,omitempty"`
+	MessageCount           *int                    `json:"message_count,omitempty"`
+	MemberCount            *int                    `json:"member_count,omitempty"`
+	ThreadMetadata         *ThreadMetaData         `json:"thread_metadata,omitempty"`
+	ThreadMember           *ThreadMember           `json:"thread_member,omitempty"`
+	AutoArchiveDuration    *int                    `json:"auto_archive_duration,omitempty"`
+	Permissions            *string                 `json:"permissions,omitempty"`
+	Flags                  *ChannelFlag            `json:"flags,omitempty"`
+	TotalMessageSent       *int                    `json:"total_message_sent,omitempty"`
+	AvailableTags          []ForumTag              `json:"available_tags,omitempty"`
+	AppliedTags            []Snowflake             `json:"applied_tags,omitempty"`
+	DefaultReactionEmoji   *DefaultReaction        `json:"default_reaction_emoji,omitempty"`
+	DefaultThreadRateLimit *int                    `json:"default_thread_rate_limit,omitempty"`
+	DefaultSortOrder       *SortOrderType          `json:"default_sort_order,omitempty"`
+	DefaultForumLayout     *ForumLayoutType        `json:"default_forum_layout,omitempty"`
 }
 
 type Overwrite struct {
-	ID    Snowflake
-	Type  int
-	Allow string
-	Deny  string
+	ID    Snowflake `json:"id"`
+	Type  int       `json:"type"`
+	Allow string    `json:"allow"`
+	Deny  string    `json:"deny"`
 }
 
 type ThreadMetaData struct {
-	IsArchived          bool
-	AutoArchiveDuration int
-	ArchiveTimestamp    time.Time
-	IsLocked            bool
-	IsInvitable         *bool
-	CreatedAt           *time.Time
+	IsArchived          bool       `json:"is_archived"`
+	AutoArchiveDuration int        `json:"auto_archive_duration"`
+	ArchiveTimestamp    time.Time  `json:"archive_timestamp"`
+	IsLocked            bool       `json:"is_locked"`
+	IsInvitable         *bool      `json:"is_invitable,omitempty"`
+	CreatedAt           *time.Time `json:"created_at,omitempty"`
 }
 
 type ThreadMember struct {
-	ID       *Snowflake
-	UserID   *Snowflake
-	JoinedAt time.Time
-	Flags    int
-	Member   GuildMember
+	ID       *Snowflake   `json:"id,omitempty"`
+	UserID   *Snowflake   `json:"user_id,omitempty"`
+	JoinedAt time.Time    `json:"joined_at"`
+	Flags    int          `json:"flags"`
+	Member   GuildMember  `json:"member"`
 }
 
 type ForumTag struct {
-	ID          Snowflake
-	Name        string
-	IsModerated bool
-	EmojiID     *Snowflake
-	EmojiName   *string
+	ID          Snowflake `json:"id"`
+	Name        string    `json:"name"`
+	IsModerated bool      `json:"is_moderated"`
+	EmojiID     *Snowflake `json:"emoji_id,omitempty"`
+	EmojiName   *string    `json:"emoji_name,omitempty"`
 }
 
 type DefaultReaction struct {
-	EmojiID   *Snowflake
-	EmojiName *string
+	EmojiID   *Snowflake `json:"emoji_id,omitempty"`
+	EmojiName *string    `json:"emoji_name,omitempty"`
 }
